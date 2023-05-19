3575762b6243033f4bbb8ecaebdc07517eeb2f31
Billy Somers <somersbilly@gmail.com>
2023-05-20 00:44:27 +1000

    Introspect model schema for records

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,27 @@
 import React from "react"
 import Link from "next/link"
+import { PrismaClient } from "@prisma/client"
 
-export default function Home() {
+export default async function Home() {
+
+  const prisma =  new PrismaClient()
+
+  // Get household records from database
+  const populationData = await prisma.population_record.findMany()
+  
+
   return (
-    <main className="flex flex-col min-h-screen items-center justify-between p-24">
-      <div className="flex flex-col items-center justify-center">
-        <h1>Main content dashboard area</h1>
-
+    <main className="flex flex-col min-h-screen items-center justify-between p-10">
+      <div className="flex flex-col container">
+        <div className="flex flex-col bg-white shadow-md rounded-md p-4">
+          <h2 className="text-2xl font-bold">Population</h2>
+          <div className="flex flex-row justify-between">
+            <div className="flex flex-col">
+              <p className="text-gray-500">Total Count</p>
+              <p className="text-2xl font-bold">{populationData.length}</p>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   )
--- components/SideNav.tsx
+++ components/SideNav.tsx
@@ -4,18 +4,8 @@ import Link from "next/link"
 
 export default function SideNav(){
     return (
-        <div className="flex flex-col w-64 h-screen px-4 py-8 bg-white border-r dark:bg-gray-800 dark:border-gray-600">
-            <div className="flex flex-col items-center mt-6 -mx-2">
-                <Image 
-                    className="object-cover w-24 h-24 mx-2 rounded-full" 
-                    src="/images/bsomers_picofme02.png" 
-                    width={30}
-                    height={30}
-                    alt="avatar" />
-                <h4 className="mx-2 mt-2 font-medium text-gray-800 dark:text-gray-200 hover:underline">Billy Somers</h4>
-                <p className="mx-2 mt-1 text-sm font-medium text-gray-600 dark:text-gray-400 hover:underline">Administrator</p>
-            </div>
-            <div className="flex flex-col justify-between flex-1 mt-6">
+        <div className="flex flex-col w-2/12 h-screen px-4 py-2 bg-white border-r dark:bg-gray-800 dark:border-gray-600">
+            <div className="flex flex-col justify-between flex-1 mt-2">
                 <nav>
                     <Link 
                         className="flex items-center px-4 py-2 text-gray-70 rounded-md dark:bg-gray-800 dark:text-gray-200" 
