399bdb81f34a94352e381eb7472baf849f44f0d2
Billy Somers <somersbilly@gmail.com>
2023-05-21 03:43:18 +1000

    Included avatar for sign in

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import Link from "next/link"
+import Image from "next/image"
 import StatsCounter from "../components/StatsCounter"
 import SummaryTable from "../components/SummaryTable"
 // import { PrismaClient } from "@prisma/client"
@@ -17,15 +18,27 @@ export default async function Home() {
 
   return (
     <div className="container px-10">
-      <div className="flex flex-row">
-        <div className="flex flex-col pt-10">
-           <h1 className="text-4xl font-bold text-cyan-950"> Dashboard </h1> 
-           <p className="text-sm font-light text-gray-500">Data used here are form testing purposes only and does not represent real data collection. Any reliance is at your own risk</p>
+      <div className="flex flex-row pt-10 w-full justify-between">
+        <div className="flex flex-col">
+          <h1 className="text-4xl font-bold text-cyan-950">Dashboard</h1>
+          <p className="text-sm font-light text-gray-500">
+            Data used here are for testing purposes only and do not represent real data collection.
+            Any reliance is at your own risk.
+          </p>
+        </div>
+
+        <div className="flex avatar online">
+          <div className="rounded-full">
+            <Image src="/images/bsomers_picofme02.png" alt="avatar" width={50} height={50} />
+          </div>
         </div>
       </div>
-      <div className="nsodivider"></div> 
-          <StatsCounter/>
-          <SummaryTable/>
-    </div>
+
+      <div className="nsodivider"></div>
+
+      <StatsCounter />
+      <SummaryTable />
+  </div>
+
   )
 }
