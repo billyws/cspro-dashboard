da442de48640e0b22882b0d602d6d90e171f2c74
Billy Somers <somersbilly@gmail.com>
2023-05-22 14:53:38 +1000

    Added some margin to daily progress graphs

--- components/DailyProgress.tsx
+++ components/DailyProgress.tsx
@@ -1,7 +1,7 @@
 export default function DailyProgress(){
     return (
         <div>
-            <div className="grid grid-cols-3 gap-4">
+            <div className="grid grid-cols-3 gap-4 mb-2">
                 <div className="p-5 my-5 rounded shadow-sm bg-slate-50">
                     <div className="flex flex-col-2 justify-between">
                         <h4 className="text-sm font-light text-gray-950">5,234</h4>
