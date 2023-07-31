3a71b19efe1e88b7fa2bc74fab0416a942629faf
Billy Somers <somersbilly@gmail.com>
2023-07-31 12:27:41 +1000

    Added fetch and auth for Survey Solutions API

---
+++ components/DateFilter.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import { useState } from "react";
+import Datepicker from "react-tailwindcss-datepicker";
+
+export default function DateFilter(){
+
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+
+    // Handle onChange
+    const handleStartChange = (value) => {
+      setStartDate(value);
+    };
+
+    const handleEndChange = (value) => {
+      setEndDate(value);
+    };
+
+    return(
+        <div className="flex flex-row">
+              <div className="flex flex-col">
+                <Datepicker 
+                    useRange={false} 
+                    asSingle={true} 
+                    value={startDate} 
+                    onChange={handleStartChange} />  
+              </div>
+              
+            </div>
+    )
+}
+
+
---
+++ components/ProvinceFilter.tsx
@@ -0,0 +1,10 @@
+
+export default function ProvinceFilter(){
+    return (
+        <div className="select select-bordered w-full flex flex-col">
+            <option disabled selected>Filter by Province</option>
+            <option>Han Solo</option>
+            <option>Greedo</option>
+        </div>
+    )
+}
