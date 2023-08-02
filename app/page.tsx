8cc56392d701071f6d35fb208948c186dc5d0f23
Billy Somers <somersbilly@gmail.com>
2023-08-03 05:08:44 +1000

    location markers added

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <div className="flex flex-row pt-10">
             <div className="grid grid-cols-3 gap-4">
               <div className="flex flex-col col-span-2">
-                  <h1 className="text-4xl font-bold text-cyan-950">Dashboard</h1>
+                  <h1 className="text-4xl font-bold text-cyan-950">Census Pilot Dashboard - DevMode</h1>
                   <p className="text-sm font-light text-gray-500">
                     Data used here are for testing purposes only and do not represent real data collection.
                     Any reliance is at your own risk.
