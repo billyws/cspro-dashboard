390f2e876f1f3e9ebad463389381f64e42d9afc4
Billy Somers <somersbilly@gmail.com>
2023-08-16 02:22:31 +1000

    fixing date filter with comments

--- components/DateFilter.tsx
+++ components/DateFilter.tsx
@@ -4,32 +4,16 @@ import { useState } from "react";
 import Datepicker from "react-tailwindcss-datepicker";
 
 export default function DateFilter(){
+  return (
+    <div>
+      Date filter here
 
-    const [startDate, setStartDate] = useState(new Date());
-    const [endDate, setEndDate] = useState(new Date());
-
-    // Handle onChange
-    const handleStartChange = (value) => {
-      setStartDate(value);
-    };
-
-    const handleEndChange = (value) => {
-      setEndDate(value);
-    };
-
-
-    return(
-        <div className="flex flex-row">
-              <div className="flex flex-col">
-                <Datepicker 
-                    useRange={false} 
-                    asSingle={true} 
-                    value={startDate} 
-                    onChange={handleStartChange} />  
-              </div>
-              
-            </div>
-    )
+    </div>
+  )
 }
+// Compare this snippet from cspro-dashboard/components/DateFilter.tsx:
+
+    
+   
 
 
